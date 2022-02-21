8f6b153b65a20b22806fd0b544fc6e2d30422cb2
Otso Björklund <otso.bjorklund@gmail.com>
2022-02-21 19:00:28 +0200

    Fix reading relative paths and paths with spaces

--- src/mncmd/score.clj
+++ src/mncmd/score.clj
@@ -6,11 +6,12 @@
            [org.wmn4j.io.musicxml MusicXmlReader])
   (:gen-class))
 
-;; TODO: Ensure that the paths are handled correctly (problems with whitespaces etc.
 (defn read-score [path]
-  (let [path-obj (java.nio.file.Paths/get (new java.net.URI (str "file://" path)))
-        reader (org.wmn4j.io.musicxml.MusicXmlReader/readerFor path-obj)]
-    (.readScore reader)))
+  ;; The into-array trick is used to just force the String param overload of Paths.get
+  (let [path-obj (java.nio.file.Paths/get path (into-array [""]))
+        abs-path (.toAbsolutePath path-obj)]
+    (with-open [reader (org.wmn4j.io.musicxml.MusicXmlReader/readerFor abs-path)]
+        (.readScore reader))))
 
 (defn- attribute-value [score attribute]
   (let [attr-opt (.getAttribute score attribute)]
