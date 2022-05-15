6a5bd04e2ca5f7b9b0d13e77df377ebadb09cb69
Otso Björklund <otso.bjorklund@gmail.com>
2022-05-15 15:19:55 +0300

    Add parts option

--- src/mncmd/score.clj
+++ src/mncmd/score.clj
@@ -9,7 +9,7 @@
   (let [path-obj (Paths/get path (into-array [""]))
         abs-path (.toAbsolutePath path-obj)]
     (with-open [reader (MusicXmlReader/readerFor abs-path)]
-        (.readScore reader))))
+      (.readScore reader))))
 
 (defn- attribute-value [score attribute]
   (let [attr-opt (.getAttribute score attribute)]
@@ -17,7 +17,7 @@
       (.get attr-opt)
       "")))
 
-(defn attributes [score]
+(defn score-attributes [score]
   (hash-map :title (attribute-value score org.wmn4j.notation.Score$Attribute/TITLE)
             :movement-title (attribute-value score org.wmn4j.notation.Score$Attribute/MOVEMENT_TITLE)
             :composer (attribute-value score org.wmn4j.notation.Score$Attribute/COMPOSER)
@@ -35,16 +35,37 @@
 (defn- rests [dur-seq]
   (filter #(instance? Rest %) dur-seq))
 
-(defn- note-count [score]
-  (let [dur-seq (score->seq score)]
-    (+ (count (notes dur-seq))
-       (apply + (map #(.getNoteCount %) (chords dur-seq))))))
+(defn- note-count [dur-seq]
+  (+ (count (notes dur-seq))
+     (apply + (map #(.getNoteCount %) (chords dur-seq)))))
 
-(defn counts [score]
+(defn score-counts [score]
   (hash-map
    :part-count (.getPartCount score)
    :measure-count (.getFullMeasureCount score)
    :has-pickup (.hasPickupMeasure score)
-   :note-count (note-count score)
+   :note-count (note-count (score->seq score))
    :rest-count (count (rests (score->seq score)))))
 
+(defn- part->dur-seq [part]
+  (let [measure-seq (iterator-seq (.iterator part))]
+    (mapcat #(iterator-seq (.iterator %)) measure-seq)))
+
+(defn part-counts [part]
+  (let [dur-seq (part->dur-seq part)]
+    (hash-map
+     :note-count (note-count dur-seq)
+     :rest-count (count (rests dur-seq)))))
+
+(defn part-attributes [part]
+  {:name (.orElse (.getName part) nil)
+   :abbr-name (.orElse (.getAttribute part org.wmn4j.notation.Part$Attribute/ABBREVIATED_NAME) nil)})
+
+(defn parts [score]
+  (iterator-seq (.iterator score)))
+
+(defn all-part-attributes [score]
+  (map part-attributes (parts score)))
+
+(defn all-part-counts [score]
+  (map part-counts (parts score)))
--- test/mncmd/score_test.clj
+++ test/mncmd/score_test.clj
@@ -5,15 +5,32 @@
 (def score (score/read-score "test/resources/twoPartsAndMeasures.musicxml"))
 
 (deftest test-attributes
-  (let [attributes (score/attributes score)]
+  (let [attributes (score/score-attributes score)]
     (testing "Test score attributes"
       (is (= "Multistaff test file" (:movement-title attributes)))
       (is (= "TestFile Composer" (:composer attributes))))))
 
 (deftest test-counts
-  (let [counts (score/counts score)]
+  (let [counts (score/score-counts score)]
     (testing "Test counts"
       (is (= 2 (:part-count counts)))
       (is (= 2 (:measure-count counts)))
       (is (= 5 (:note-count counts)))
       (is (= 1 (:rest-count counts))))))
+
+(deftest test-parts
+  (let [parts (score/all-part-attributes score)]
+    (testing "Test parts"
+      (is (= 2 (count parts)))
+      (is (= "Part1" (:name (nth parts 0))))
+      (is (= "Part2" (:name (nth parts 1)))))))
+
+(deftest test-part-counts
+  (let [part-counts (score/all-part-counts score)
+        part-one-counts (nth part-counts 0)
+        part-two-counts (nth part-counts 1)]
+    (testing "Test part counts"
+      (is (= 3 (:note-count part-one-counts)))
+      (is (= 1 (:rest-count part-one-counts)))
+      (is (= 2 (:note-count part-two-counts)))
+      (is (= 0 (:rest-count part-two-counts))))))
