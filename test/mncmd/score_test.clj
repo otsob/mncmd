101c55dc9c535a5f32a5919438d060ba8357a3e3
Otso Björklund <otso.bjorklund@gmail.com>
2022-08-20 18:14:30 +0300

    Update test file
    Add more details to test file
    to ensure more execution paths are
    executed.

--- test/mncmd/score_test.clj
+++ test/mncmd/score_test.clj
@@ -2,7 +2,7 @@
   (:require [clojure.test :refer :all]
             [mncmd.score :as score]))
 
-(def score (score/read-score "test/resources/twoPartsAndMeasures.musicxml"))
+(def score (score/read-score "test/resources/multistaff_test_file.musicxml"))
 
 (deftest test-attributes
   (let [attributes (score/score-attributes score)]
@@ -13,24 +13,29 @@
 (deftest test-counts
   (let [counts (score/score-counts score)]
     (testing "Test counts"
-      (is (= 2 (::score/part-count counts)))
+      (is (= 3 (::score/part-count counts)))
       (is (= 2 (::score/measure-count counts)))
-      (is (= 5 (::score/note-count counts)))
-      (is (= 1 (::score/rest-count counts))))))
+      (is (= 11 (::score/note-count counts)))
+      (is (= 5 (::score/rest-count counts))))))
 
 (deftest test-parts
   (let [parts (score/all-part-attributes score)]
     (testing "Test parts"
-      (is (= 2 (count parts)))
+      (is (= 3 (count parts)))
       (is (= "Part1" (::score/part-name (nth parts 0))))
-      (is (= "Part2" (::score/part-name (nth parts 1)))))))
+      (is (= "Part2" (::score/part-name (nth parts 1))))
+      (is (= "Piano" (::score/part-name (nth parts 2))))
+      (is (= "Pno." (::score/abbr-name (nth parts 2)))))))
 
 (deftest test-part-counts
   (let [part-counts (score/all-part-counts score)
         part-one-counts (nth part-counts 0)
-        part-two-counts (nth part-counts 1)]
+        part-two-counts (nth part-counts 1)
+        part-three-counts (nth part-counts 2)]
     (testing "Test part counts"
-      (is (= 3 (::score/note-count part-one-counts)))
+      (is (= 5 (::score/note-count part-one-counts)))
       (is (= 1 (::score/rest-count part-one-counts)))
       (is (= 2 (::score/note-count part-two-counts)))
-      (is (= 0 (::score/rest-count part-two-counts))))))
+      (is (= 0 (::score/rest-count part-two-counts)))
+      (is (= 4 (::score/note-count part-three-counts)))
+      (is (= 4 (::score/rest-count part-three-counts))))))
