b3213066482c04473fb7878a5d63763c5406e1c0
Otso Björklund <otso.bjorklund@gmail.com>
2022-08-26 17:46:45 +0300

    Add native build config for ambitus

--- native-build/generate-resource-files.sh
+++ native-build/generate-resource-files.sh
@@ -3,4 +3,4 @@
 # Requires setting JAVA_HOME to GraalVM Home directory
 
 lein uberjar
-$JAVA_HOME/bin/java -agentlib:native-image-agent=config-output-dir=./native-build/ -jar ./target/mncmd-0.1.1-standalone.jar stat ./test/resources/multistaff_test_file.musicxml --counts --parts
+$JAVA_HOME/bin/java -agentlib:native-image-agent=config-output-dir=./native-build/ -jar ./target/mncmd-0.1.1-standalone.jar stat ./test/resources/multistaff_test_file.musicxml --counts --parts --ambitus
