4f9dda8242c8eaaee0fe57384f2a069091e5a584
Otso Björklund <otso.bjorklund@gmail.com>
2022-08-29 20:32:25 +0300

    Bump version: 0.1.1 → 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mncmd "0.1.1"
+(defproject mncmd "0.1.2"
   :description "Command line tool for extracting information from music notation files"
   :url "https://github.com/otsob/mncmd"
   :license {:name "MIT License"
