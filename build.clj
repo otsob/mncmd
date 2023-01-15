25ee545c7b2d58b6cb6bc88d3d380b8b3c42a510
Otso Björklund <otso.bjorklund@gmail.com>
2023-01-15 13:59:53 +0200

    Use clojure tools for build
    Remove leiningen

---
+++ build.clj
@@ -0,0 +1,23 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]))
+
+(def project 'project/mncmd)
+(def version "0.1.6")
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def uber-file (format "target/%s-%s-standalone.jar" (name project) version))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn uberjar [_]
+  (clean nil)
+  (b/copy-dir {:src-dirs ["src"]
+               :target-dir class-dir})
+  (b/compile-clj {:basis basis
+                  :src-dirs ["src"]
+                  :class-dir class-dir})
+  (b/uber {:class-dir class-dir
+           :uber-file uber-file
+           :basis basis
+           :main 'mncmd.core}))
